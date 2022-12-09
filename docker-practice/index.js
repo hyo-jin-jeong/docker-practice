f91debf1342da8860476404f7ed63a3bce340238
hyo-jin-jeong <55984573+hyo-jin-jeong@users.noreply.github.com>
2022-12-09 16:59:01 +0900

    Docker 연습

---
+++ docker-practice/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Docker Practice!");
+});
+
+app.listen(8080, () => {
+  console.log("server start");
+});
